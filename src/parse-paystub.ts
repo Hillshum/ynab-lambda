4056a76a5a2a10e010afe9d0567a263ac1fd6c20
Hilton Shumway <hillshum@gmail.com>
2020-05-04 22:23:11 -0500

    Add TDD framework and parse paystub

---
+++ src/parse-paystub.ts
@@ -0,0 +1,40 @@
+
+
+const regex = {
+  gross: /^ Gross Pay\$(.+)$/m,
+  taxes: /^ EE Taxes-\$(.+)$/m,
+  hsa: /^\*HEALTH SAVINGS ACCT\n-\$(.+)$/m,
+  health: /^\*HEALTH CARE\n-\$(.+)$/m,
+  retirement: /^\*RSP PRE-TAX BASIC\n-\$(.+)$/m,
+  net: /^ Net Pay\$(.+)$/m,
+
+}
+
+interface ParsedStub {
+  gross: number,
+  taxes: number,
+  hsa: number,
+  health: number,
+  retirement: number,
+  postTax: number,
+  net: number,
+}
+
+const checkRegex = (regex: RegExp, corpus: string) => {
+  const match = corpus.match(regex);
+  const str = match && match[1] || null;
+  const result = Number(str?.replace(',', ''));
+  return result
+}
+
+export const parsePaystub = (stub: string): ParsedStub => {
+  return Object.entries(regex).reduce((prev, [key, reg]) => {
+    const match = checkRegex(reg, stub);
+
+    if (isNaN(match)) {
+      throw new Error(`Unable to parse ${key} from ${stub}`);
+    }
+
+    return {...prev, [key]: match};
+  }, {}) as ParsedStub;
+}
