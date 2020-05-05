de7fef18aba3522b9e9309796a63989a8d20f5fa
Hilton Shumway <hillshum@gmail.com>
2020-05-04 22:27:49 -0500

    Restructure index for multiple lambdas

---
+++ src/functions/venmo.ts
@@ -0,0 +1,38 @@
+import { Handler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import * as api from '../utils/api'
+import { parse } from '../utils/csv';
+import { SaveTransactionsResponse } from 'ynab';
+
+
+require('portable-fetch');
+
+const handler: Handler<APIGatewayProxyEvent, APIGatewayProxyResult> = async (event) => {
+
+  if (! event.body) {
+    throw new Error("Missing POSTBODY");
+  }
+
+  const csvTransactions = parse(event.body);
+
+  const existingTransactions = await api.loadTransactions();
+  console.log(existingTransactions);
+
+  const existingIds = new Set(existingTransactions.map(({import_id})=>import_id));
+
+  const toUpload = csvTransactions.filter(({ImportId})=>!existingIds.has(ImportId))
+
+  let result: SaveTransactionsResponse | null = null;
+
+  if (toUpload.length) {
+    result = await api.postTransactions(toUpload);
+  }
+    
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(result)
+  }
+
+  return response;
+}
+
+export { handler}
