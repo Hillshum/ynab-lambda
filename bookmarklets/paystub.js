a9806708b1071905022a2318692ea70d130201fc
Hilton Shumway <hillshum@gmail.com>
2020-12-16 22:43:14 -0600

    Show results in bookmarklet

--- bookmarklets/paystub.js
+++ bookmarklets/paystub.js
@@ -7,6 +7,13 @@ const path = 'https://5m5oojlwpe.execute-api.us-east-1.amazonaws.com/test/paystu
 fetch(path, {
   method: 'POST',
   body: text
+}).then(res => {
+  const div = document.createElement('div');
+  const color = res.ok ? 'green' : 'red';
+  div.setAttribute('style', `position: fixed; top: 20px; right: 50%; height: 2em; width: 200px; background-color: ${color};`);
+  div.innerText = res.ok ? 'Success' : 'Error';
+  document.body.appendChild(div);
+  setTimeout(() => document.body.removeChild(div), 5000);
 })
 
 }())
