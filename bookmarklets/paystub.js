4087dee46314f9476211ba141cfcb578fd1eed0f
Hilton Shumway <hillshum@gmail.com>
2020-12-31 13:30:06 -0600

    Bookmarklet gets stub from XHR

--- bookmarklets/paystub.js
+++ bookmarklets/paystub.js
@@ -1,19 +1,23 @@
 
 (function(){
-const text = window.getSelection().toString();
 
 const path = 'https://5m5oojlwpe.execute-api.us-east-1.amazonaws.com/test/paystub';
 
-fetch(path, {
-  method: 'POST',
-  body: text
-}).then(res => {
-  const div = document.createElement('div');
-  const color = res.ok ? 'green' : 'red';
-  div.setAttribute('style', `position: fixed; top: 20px; right: 50%; height: 2em; width: 200px; background-color: ${color};`);
-  div.innerText = res.ok ? 'Success' : 'Error';
-  document.body.appendChild(div);
-  setTimeout(() => document.body.removeChild(div), 5000);
+fetch( 'https://portal001.globalview.adp.com/gvservice/gm/ess/pay/overview').then(res =>{
+  return res.text()
+}).then(text => {
+
+  fetch(path, {
+    method: 'POST',
+    body: text
+  }).then(res => {
+    const div = document.createElement('div');
+    const color = res.ok ? 'green' : 'red';
+    div.setAttribute('style', `position: fixed; top: 20px; right: 50%; height: 2em; width: 200px; background-color: ${color};`);
+    div.innerText = res.ok ? 'Success' : 'Error';
+    document.body.appendChild(div);
+    setTimeout(() => document.body.removeChild(div), 5000);
+  })
 })
 
 }())
