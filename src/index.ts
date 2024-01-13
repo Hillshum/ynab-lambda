2ed9850624b9fefb1a53ffbbafbb324decbfeb00
Hilton Shumway <hillshum@gmail.com>
2024-01-13 08:57:20 -0600

    Add express shell

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
+import express from 'express';
 import { handler as venmo } from './functions/venmo';
 import paystubHandler from './functions/paystub';
 
 import lambdaWrapper from './utils/lambda-wrapper';
+const paystub = lambdaWrapper(paystubHandler)
+const port = process.env.PORT || 3000;
 
-// import localValidator from './local-validator'
+const app = express();
 
-// localValidator()
-const paystub = lambdaWrapper(paystubHandler);
-export { venmo, paystub };
+// Venmo endpoint
+app.post('/venmo', venmo);
+
+// Paystub endpoint
+app.post('/paystub', paystub );
+
+app.listen(port, () => {
+    console.log('Server is running on port 3000');
+});
+
+export { paystub, venmo}
